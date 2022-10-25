446aafa0ee1b3f43c86ced141a54dba7d3287a4d
Callum McLuskey <callummcluskey100@gmail.com>
2022-10-25 13:49:27 +0100

    updated typescript template with splash screen

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {DeviceController} from '@espruino-tools/device-controller'
-import './styles/app.scss'
-const device = new DeviceController();
+import { showTemplate } from "./espruino-template";
+import "./styles/app.scss";
 
-export const connect = () => device.connect()
-
+window.onload = function () {
+  showTemplate();
+};
